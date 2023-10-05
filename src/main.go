949a0375dac776db70fb641e62a4281fd5bbd7a8
bloodgroup-cplusplus <tucater10896601@protonmail.com>
2023-10-05 09:59:51 +0530

    added a function to crate new load balancer

--- src/main.go
+++ src/main.go
@@ -42,8 +42,34 @@ func newSimpleServer(addr string) *simpleServer {
 	}
 }
 
+func (lb *LoadBalancer) getNextAvailableServer() Server {
+
+}
+
+func (lb *LoadBalancer) serveProxy(rw http.ResponseWriter, r *http.Request) {
+
+}
+
 func handleErr(err error) {
 	if err != nil {
 		fmt.Printf("error: %v\n", err)
 	}
+}
+
+func main() {
+	// create a list of servers
+	servers := []Server{
+		newSimpleServer("https://www.facebook.com"),
+		newSimpleServer("https://www.bing.com"),
+		newSimpleServer("https://www.duckduckgo.com"),
+	}
+	lb := NewLoadBalancer("8000", servers)
+	handleRedirect := func(rw http.ResponseWriter, req *http.Request) {
+		lb.serveProxy(rw, req)
+	}
+	http.HandleFunc("/", handleRedirect)
+
+	fmt.Printf("server request at localhost:%s\n", lb.port)
+	http.ListenAndServe(":"+lb.port, nil)
+
 }
